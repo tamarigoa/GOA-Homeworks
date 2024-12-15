a2a929e4a49a0758cc8205bf6d804e6cc6ee7f54
LukaNaveriani <lukanaveriani95@gmail.com>
2024-12-15 23:51:45 +0400

    commit

---
+++ day 31/homework/hmwrk.py
@@ -0,0 +1,26 @@
+# def find_short(s):
+#     words = s.split()
+#     shortest_length = min(len(word) for word in words)
+#     return shortest_length
+
+
+
+# def is_isogram(s):
+#     s = s.lower()
+#     letters = set()
+#     for letter in s:
+#         if letter in letters:
+#             return False
+#         letters.add(letter)
+#     return True
+
+
+
+
+
+# def validate_pin(pin):
+#     return len(pin) in [4, 6] and pin.isdigit()
+
+
+
+
